af6ef857183be3141c6c8b5060e4e0e52b98c939
fiddle-thomas <code.fiddle@gmail.com>
2024-06-28 23:50:11 +0530

    Update mad_libs_game.py

--- mad_libs_game.py
+++ mad_libs_game.py
@@ -1,1 +1,19 @@
-# Mad Libs game logic will be implemented here
+adj_1 = input("Adjective: ")
+adj_2 = input("Adjective: ")
+noun_1 = input("Noun: ")
+noun_2 = input("Noun: ")
+pl_noun_1 = input("Plural Noun: ")
+verb_ing_1 = input("Verb ending in 'ing': ")
+pl_noun_2 = input("Plural Noun: ")
+vern_ing_2 = input("Verb ending in 'ing': ")
+noun_3 = input("Noun: ")
+plant_1 = input("Plant: ")
+part_of_body_1 = input("Part of the body: ")
+place_1 = input("A place: ")
+verb_ing_3 = input("Verb ending in 'ing': ")
+num_1 = input("Number: ")
+pl_noun_3 = input("Plural Noun: ")
+
+madlib = f"A vacation is when you take a trip to some {adj_1} place with your {adj_2} family. Usually you go to some place that is near a/an {noun_1} or up on a/an {noun_2}. A good vacation place is one where you can ride {pl_noun_1}. I like to spend my time {verb_ing_1}. When parents go on a vacation, they spend their time eating three {pl_noun_2} a day, and fathers play golf, and mothers sit around {vern_ing_2}. Last summer, my little brother fell in a/an {noun_3} and got poison {plant_1} all over his {part_of_body_1}. My family is going to go to (the) {place_1}, and I will practice {verb_ing_3}. Parents need vacations more than kids because they have to work {num_1} hours every day all year making enough {pl_noun_3} to pay for the vacation."
+
+print(madlib)
